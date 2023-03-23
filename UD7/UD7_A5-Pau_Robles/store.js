9e19c29955f127b768e1e7216f5c2e4890c89164
PRobles_dwec <probles19088@iesjoanramis.org>
2023-03-23 17:23:26 +0100

    Pinia add

--- UD7/UD7_A5-Pau_Robles/store.js
+++ UD7/UD7_A5-Pau_Robles/store.js
@@ -1,19 +1,41 @@
 const store = Pinia.defineStore('counter', {
     state: () => ({ 
-      count: 0,
-      list: [] 
+      users: [], // Array with the users.
+      user_logged: null  // The id by default will be null.
     }),
+    getters: {
+        getUser: function (state) { // // Method to get an specific user.
+            return function(userId) {
+                return state.users.filter(u => u.id === userId );
+            }            
+        },
+        getUsers: function (state) { // Method to get all the users.
+            return state.users;
+        },        
+    },
     actions: {
-      increment() {
-        this.count++;
+      init() {
+        console.log("Starting Pinia DB.");
+        try {
+            this.users = JSON.parse(localStorage.getItem("users"));
+        } catch (e) {
+            this.users = [];
+        }
+        console.log(this.users);
+        if(this.users == null) {
+            this.users = [];
+        }
+        try {
+            this.user_logged = JSON.parse(localStorage.getItem("user_logged"));
+        } catch (ex) {
+            this.user_logged = null;
+        }
+        console.log(this.user_logged);
+        
       },
-      addItem:function(item){
-        this.list.push(item);
+      addItem: function(item) {
+        this.users.push(item);
       },
-      deleteItem:function(item){
-        let position = this.list.indexOf(item);
-        this.list.splice(position, 1);
-      }
     },
   });
   
